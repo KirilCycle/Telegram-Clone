6efa6ed91fe3479a4d87cad117d47c5c1346863f
KirilCycle <vtkkllx2367@gmail.com>
2023-04-08 21:20:18 +0300

    basic layout, with icons and actions/ msg-actions

--- src/components/MessageActions.vue
+++ src/components/MessageActions.vue
@@ -1,56 +1,106 @@
 <template>
-    <Teleport  to="body">
-        
-        <div class="msg-action-wrap">
-            <div ref="modal" class="msg-actions">
-                
-            </div>
-        </div>
-  
-    </Teleport>
+  <Teleport to="body">
+    <div class="msg-action-wrap">
+      <div ref="modal" class="msg-actions">
+        <ul class="actions-list">
+          <li>
+            <span class="material-symbols-outlined"> reply </span>
+            <button>Reply</button>
+          </li>
+          <li>
+            <span class="material-symbols-outlined"> delete </span>
+            <button>Delete</button>
+          </li>
+          <li>
+            <span class="material-symbols-outlined">
+                forward
+                </span>
+            <button>Forward</button>
+          </li>
+          <li>
+            <span class="material-symbols-outlined">
+                file_copy
+                </span>
+            <button>Coppy Text</button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </Teleport>
 </template>
 
 <script>
-import store from '@/store/store';
-    export default {
-    
-        data() {
-             return {
+import store from "@/store/store";
+export default {
+  data() {
+    return {};
+  },
 
-             }
+  mounted() {
+    console.log(
+      store.state.message.coords.x,
+      store.state.message.coords.y,
+      "AAA"
+    );
 
-        },
-        
+    // this.$refs.modal.style.transform  = `translate(${store.state.message.coords.x}}px,${store.state.message.coords.y}px)`
 
-        mounted() {
-            console.log(store.state.message.coords.x, store.state.message.coords.y, 'AAA')
-            
-            // this.$refs.modal.style.transform  = `translate(${store.state.message.coords.x}}px,${store.state.message.coords.y}px)`
-
-            this.$refs.modal.style.transform = `translate(${ store.state.message.coords.x  }px, ${ store.state.message.coords.y}px)`
-            // this.$refs.modal.style.transform = `translateY(${ -store.state.message.coords.y  }px)`
-        }
-    }
-    
+    this.$refs.modal.style.transform = `translate(${store.state.message.coords.x}px, ${store.state.message.coords.y}px)`;
+    // this.$refs.modal.style.transform = `translateY(${ -store.state.message.coords.y  }px)`
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 .msg-action-wrap {
-
-    width: 100%;
-    height: 100%;
-    position: fixed;
-    background-color: #464646a6;
-
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  background-color: #4646462e;
 }
 
 .msg-actions {
-    top:0%;
-    width: 100px;
-    height: 100px;
-    background-color: #fff;
-    position: absolute;
+  top: 0%;
+  width: 200px;
+  border-radius: 5px;
+  background-color: #353535f1;
+  position: absolute;
+  backdrop-filter: blur(4px);
+  height: min-content;
+  padding: 5px;
 
+  .actions-list {
+    width: 100%;
+    font-size: 1rem;
+    font-weight: normal;
+    box-sizing: border-box;
+
+    li {
+      font-family: Avenir, Helvetica, Arial, sans-serif;
+      -webkit-font-smoothing: antialiased;
+      color: #fff;
+      padding: 9px 11px 9px 11px;
+      box-sizing: border-box;
+      display: flex;
+      border-radius: 3px;
+      align-items: center;
+      flex-direction: row;
+      cursor: pointer;
+
+      button {
+        height: 100%;
+        padding: 10px;
+      }
+
+      span {
+        font-size: 0.8rem sans-serif;
+        font-weight: 300;
+      }
+
+      &:hover {
+        background-color: rgb(41, 41, 41);
+      }
+    }
+  }
 }
-
 </style>
